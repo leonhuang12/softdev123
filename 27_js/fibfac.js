3fddbd01c1ad3b48e217b479837f7826ea192f51
Leon Huang <lhuang50@stuy.edu>
2025-01-06 21:13:10 -0500

    wk27 added

--- 27_js/fibfac.js
+++ 27_js/fibfac.js
@@ -1,5 +1,5 @@
-//Team Phantom Tollbooth :: Clyde Sinclair, Fierce Dragon
-//SoftDev pd0
+//Team KungFuPandaSquad/2 :: Evan Chan and Leon Huang
+//SoftDev pd4
 //K27 - Basic functions in JavaScript
 //2025-01-06m
 
@@ -8,6 +8,16 @@
 //factorial:
 
 //<your team's fact(n) implementation>
+
+var fact = fac(n){
+if(n<0){
+    return 0;
+  }
+  else if(n==0){
+    return 1;
+  }
+  else return n*fac(n-1);
+}
 
 //TEST CALLS
 // (writing here can facilitate EZer copy/pasting into dev console now and later...)
@@ -20,8 +30,17 @@
 
 //<your team's fib(n) implementation>
 
+var fibo = fib(n){
+if(n==0){
+    return 0;
+}
+else if(n==1){
+    return 1;
+}
+else return fib(n-1)+fib(n-2)
+}
+
 //TEST CALLS
 // (writing here can facilitate EZer copy/pasting into dev console now and later...)
 
 //=================================================================
-
---
+++ 27_js/fibfac.scm
@@ -0,0 +1,44 @@
+;Team KungFuPandaSquad/2 :: Evan Chan and Leon Huang
+;SoftDev pd4
+;K27 - Basic functions in JavaScript
+;2025-01-06m
+
+;Scheme antecedents for JavaScript work
+
+
+;factorial:
+(define fact (lambda (n)
+;<your team's fact(n) implementation>
+    (if (= n 0)
+        1
+        (* n (fact (- n 1))))))
+
+;TEST CALLS
+(fact 1) ;"...should be  1"
+(fact 2) ;"...should be  2"
+(fact 3) ;"...should be  6"
+(fact 4) ;"...should be  24"
+(fact 5) ;"...should be  120"
+
+
+;-----------------------------------------------------------------
+
+
+;fib:
+;<your team's fib(n) implementation>
+
+(define fib(lambda (n)
+;<your team's fib(n) implementation>
+    (if (<= n 1)
+        n
+        (+ (fib (- n 1)) (fib (- n 2))))))
+
+
+;TEST CALLS
+(fib 0) ;"...should be  0"
+(fib 1) ;"...should be  1"
+(fib 2) ;"...should be  1"
+(fib 3) ;"...should be  2"
+(fib 4) ;"...should be  3"
+
+;=================================================================
